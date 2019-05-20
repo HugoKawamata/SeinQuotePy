7234bc36cc1220b71034d4917af842594619003f
Daniel Gilchrist <dan@tanda.co>
2019-05-20 13:57:28 +1000

    Fix issues after site was changed
    Pages were no longer accessible due to "mod_security" which prevents loading content without cookies so we used mechanize to get around that. We also now use beautiful soup for easier parsing.

--- seinquote.py
+++ seinquote.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-from lxml import html
-import requests
+from bs4 import BeautifulSoup
+import mechanize
 import urls
 
 
@@ -12,29 +12,37 @@ if len(sys.argv) != 2:
 else:
     quot = sys.argv[1]
 
+browser = mechanize.Browser()
 trueEps = []
 
 print("Searching scripts...\n")
 for episode in urls.eps:
-    # Get all the text from the document
-    page = requests.get(episode.url)
-    tree = html.fromstring(page.content)
-    content = tree.xpath('//div[@id="content"]//p')
-    text = ["".join(item.xpath('.//text()')) for item in content]
-    if len(text) == 0:
-        print("Something's wrong with the formatting of episode " + 
-        episode.name)
-    final = ""
-    for item in text:
-        final += (item)
+    try:
+        # Get all the text from the document
+        page = browser.open(episode.url)
+        soup = BeautifulSoup(page.read(), features="lxml")
 
-    # Scan it for instances of quot
-    if quot in final:
-        trueEps.append(episode)
-        print("□", end="")
-    else:
-        print("■", end="")
-    sys.stdout.flush()
+        content = soup.find("div", { "id": "content" })
+        text = [p.text.lower() for p in content.find_all("p")]
+
+        if len(text) == 0:
+            print("Something's wrong with the formatting of episode", episode.name)
+
+        final = ""
+        for item in text:
+            final += (item)
+
+        # Scan it for instances of quot
+        if quot.lower() in final:
+            trueEps.append(episode)
+            print("□", end="")
+        else:
+            print("■", end="")
+        sys.stdout.flush()
+    except:
+        print(episode.name)
+        pass
 print("\n")
+
 for episode in trueEps:
     print(episode.__repr__())
